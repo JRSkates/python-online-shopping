b9e28d2f0feaae94f533048959b3e0b8edba7779
JRSkates <skatesproduction@gmail.com>
2023-10-17 15:45:17 +0100

    Final Pytest Complete

--- test_product_and_customer.py
+++ test_product_and_customer.py
@@ -5,3 +5,18 @@ def test_product_details():
   product = Product(1, "Laptop", 800)
   assert product.get_details() == "Laptop - £800"
 
+def test_customer_operations():
+  product1 = Product(1, "Mouse", 30)
+  product2 = Product(2, "Keyboard", 50)
+  customer = Customer(1, "Jack", "jack@email.com")
+
+  customer.add_to_cart(product1)
+  customer.add_to_cart(product2)
+
+  cart_items = customer.view_cart()
+  assert len(cart_items) == 2
+  assert "Mouse - £30" in cart_items
+  assert "Keyboard - £50" in cart_items
+
+  total_price = customer.checkout()
+  assert total_price == 80
