5194f6899f7f24cf871e629a7af4d06ef4123610
JRSkates <skatesproduction@gmail.com>
2023-10-17 15:59:13 +0100

    Customer Class Updated to work with new Order and ShoppingCart Classes

--- customer.py
+++ customer.py
@@ -1,16 +1,23 @@
+from shopping_cart import ShoppingCart
+from order import Order
+
 class Customer:
   def __init__(self, customer_id, name, email):
     self.customer_id = customer_id
     self.name = name
     self.email = email
-    self.cart = []
+    self.shopping_cart = ShoppingCart()
+    self.orders = []
 
-  def add_to_cart(self, product):
-    self.cart.append(product)
+  def add_to_cart(self, product, quantity=1):
+    self.cart.append(product, quantity)
 
   def view_cart(self):
-    return [product.get_details() for product in self.cart]
+    return self.shopping_cart.view_cart()
   
   def checkout(self):
-    total_price = sum(product.price for product in self.cart)
-    return total_price
+    total_price = self.shopping_cart.get_total_price()
+    new_order = Order(len(self.orders) + 1, self, self.shopping_cart.items, total_price)
+    self.orders.append(new_order)
+    self.shopping_cart = ShoppingCart()
+    return new_order
